fd6b8e71204fb2523a453412cd1fe02d6d7e4049
victorlazaro <12ohcutiv>
2017-10-10 22:48:31 -0600

    Finished project

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+from prime_generator import generate_large_prime
 
 def extended_gcd(a, b):
-    x0, x1, y0, y1 = 1, 0, 0, 1
-    while b != 0:
-        q, a, b = a // b, b, a % b
-        x0, x1 = x1, x0 - q * x1
-        y0, y1 = y1, y0 - q * y1
-    return a, x0, y0
+    x, y, u, v = 0, 1, 1, 0
+    while a != 0:
+        q, r = b // a, b % a
+        m, n = x - u * q, y - v * q
+        b, a, x, y, u, v = a, r, u, v, m, n
+    gcd = b
+    return gcd, x, y
 
 
 def mod_exp(g, s, p):
@@ -20,13 +22,22 @@ def mod_exp(g, s, p):
 
 
 def main():
-    # g = 5
-    # s = generate_large_prime(500)
-    # p = generate_large_prime(500)
-    # moded = mod_exp(g, s, p)
 
-    # print('s = {}\np = {}\nmoded = {}'.format(s, p, moded))
-    extended_gcd(240, 46)
+    e = 65537
+
+    while True:
+        p = generate_large_prime(512)
+        q = generate_large_prime(512)
+        gcd, x, y = extended_gcd((p-1)*(q-1), e)
+        if gcd == 1:
+            break
+    print("gcd: ", gcd)
+    print("x: ", x)
+    print("y: ", y)
+    print("p: ", p)
+    print("q: ", q)
+    print("p*q: ", p*q)
+
 
 if __name__ == "__main__":
     main()
